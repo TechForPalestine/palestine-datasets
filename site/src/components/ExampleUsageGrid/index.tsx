a20f724dfdb5586d71805202d9b17aa9fa2018b1
Wes Johnson <wes@swj.io>
2024-02-07 20:18:22 -0500

    improve example usage page

---
+++ site/src/components/ExampleUsageGrid/index.tsx
@@ -0,0 +1,44 @@
+import clsx from "clsx";
+import styles from "./styles.module.css";
+import { useMemo } from "react";
+
+type Props = {
+  examples: Array<{ image: string; description: string; link: string }>;
+};
+
+const numberPerRow = 2;
+
+export const ExampleUsageGrid = (props: Props) => {
+  const rows = useMemo(() => {
+    return props.examples.reduce((acc, example) => {
+      const newRow = Number.isInteger(acc.length / numberPerRow);
+      if (newRow) {
+        acc.push([example]);
+      } else {
+        acc[acc.length - 1].push(example);
+      }
+      return acc;
+    }, [] as Array<Props["examples"]>);
+  }, [props.examples]);
+
+  return (
+    <div>
+      {rows.map((row) => (
+        <div className="row">
+          {row.map((col) => (
+            <div className={clsx("col col--6")}>
+              <a href={col.link} target="_blank">
+                <div className={styles.exampleCard}>
+                  <img src={col.image} alt={col.description} />
+                  <div className={styles.exampleCardBody}>
+                    {col.description}
+                  </div>
+                </div>
+              </a>
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
--- site/src/components/index.ts
+++ site/src/components/index.ts
@@ -1,5 +1,6 @@
 export * from "./DailyExplorer";
 export * from "./DailyReportStatsLabel";
+export * from "./ExampleUsageGrid";
 export * from "./ExternalLinkButton";
 export * from "./JSONFileLinks";
 export * from "./KilledListCountLabel";
