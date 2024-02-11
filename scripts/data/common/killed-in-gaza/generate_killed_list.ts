646e96651c1b55e1e912eaaba52b27be6c2dbcb1
Wes Johnson <wes@swj.io>
2024-02-11 11:54:41 -0500

    add script that handles ar->ar segments

---
+++ scripts/data/common/killed-in-gaza/generate_killed_list.ts
@@ -0,0 +1,63 @@
+import fs from "fs";
+
+const pwd = "scripts/data/common/killed-in-gaza";
+const arRawNameColumnLabel = "name_ar_raw";
+const arEnNameColumnLabel = "name_en";
+
+const readCsv = (repoPath: string) => {
+  const csvString = fs.readFileSync(repoPath).toString();
+  return csvString.split(/\r?\n/g).map((row) => row.split(","));
+};
+
+/**
+ * read a CSV file and return an object lookup ("dict") with keys
+ * as the first CSV column value, and values as the second CSV column
+ */
+const readCsvToDict = (repoPath: string) => {
+  return readCsv(repoPath).reduce(
+    (dict, row) => ({
+      ...dict,
+      [row[0]]: row[1],
+    }),
+    {} as Record<string, string>
+  );
+};
+
+const rawList = readCsv(`${pwd}/data/raw.csv`);
+const arToAr = readCsvToDict(`${pwd}/data/dict_ar_ar.csv`);
+const arToEn = readCsvToDict(`${pwd}/data/dict_ar_en.csv`);
+
+const [rawHeaderRow, ...rawListRows] = rawList;
+const arRawColumn = rawHeaderRow.indexOf(arRawNameColumnLabel);
+if (arRawColumn === -1) {
+  throw new Error(`Expected raw list column named "${arRawNameColumnLabel}"`);
+}
+
+/**
+ * splits the full name into segments and replaces each segment with
+ * matching values from dict, otherwise keeps the unmatched segment
+ * @param name full name
+ * @param dict lookup used to swap each name segment
+ * @returns full name string
+ */
+const replaceNameSegments = (name: string, dict: Record<string, string>) => {
+  return name
+    .split(/\s+/)
+    .map((segment) => dict[segment] ?? segment)
+    .join(" ");
+};
+
+const resultList = rawListRows.map((row) => {
+  const arName = replaceNameSegments(row[arRawColumn], arToAr);
+  row[arRawColumn] = arName;
+  // append name_en col value
+  return [...row, replaceNameSegments(arName, arToEn)];
+});
+
+const toCsv = (list: string[][]) => list.map((row) => row.join(",")).join("\n");
+
+const newHeaders = [...rawHeaderRow, arEnNameColumnLabel].join(",");
+fs.writeFileSync(
+  `${pwd}/output/result.csv`,
+  `${newHeaders}\n${toCsv(resultList)}`
+);
