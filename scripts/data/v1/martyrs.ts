80e079c93fe55bdd6a89d6e39fd030046add2b4d
Wes Johnson <wes@swj.io>
2024-02-23 15:53:00 -0500

    rm unused scripts for v1 apis

--- scripts/data/v1/martyrs.ts
+++
@@ -1,6 +0,0 @@
-import { writeJson } from "../../utils/fs";
-import { ApiResource } from "../../../types/api.types";
-
-const martyrs = require("../../../old/martyrs.json");
-
-writeJson(ApiResource.KilledInGazaV1, "old/martyrs.json", martyrs, true);
