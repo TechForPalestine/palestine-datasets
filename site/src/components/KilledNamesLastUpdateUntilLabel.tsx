b82169b05ea366bd88802ed6abbeae16f2535451
Wes Johnson <wes@swj.io>
2025-09-27 08:01:05 -0400

    fix date issue

--- site/src/components/KilledNamesLastUpdateUntilLabel.tsx
+++ site/src/components/KilledNamesLastUpdateUntilLabel.tsx
@@ -3,7 +3,9 @@ import { format } from "date-fns/format";
 import { updateDates } from "../../../scripts/data/common/killed-in-gaza/constants";
 
 const lastUpdate = updateDates[updateDates.length - 1];
+const isoStringSuffix = `T12:00:00.000Z`;
 
 export const KilledNamesLastUpdateUntilLabel = () => {
-  return <span>{format(lastUpdate.includesUntil, "PPP")}</span>;
+  const labelDt = new Date(`${lastUpdate.includesUntil}${isoStringSuffix}`);
+  return <span>{format(labelDt, "PPP")}</span>;
 };
