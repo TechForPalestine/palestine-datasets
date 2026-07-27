a2513625502ffffc1bfdb6850c124b9d2f8eb17b
Wes Johnson <wes@swj.io>
2026-07-27 18:44:32 -0400

    fix(ci): generate v3 KiG list in workflow

--- scripts/data/common/killed-in-gaza/constants.ts
+++ scripts/data/common/killed-in-gaza/constants.ts
@@ -21,11 +21,7 @@ export const canonicalUpdateCommits = [
   "b936c35ff3556d31df0833815456b9820b4882c8", // 7: June/July 2025 (IBC)
   "4e95d05d79fffe232d7e551a89e3913199addf46", // 8: July 2025 (IBC)
   "68a207a49227514b0822f8816add4415718ec172", // 9: August 2025 (IBC)
-  // TODO(10): fill in with this update's squashed commit on main once merged.
-  // It has to be a commit reachable on main -- killed-in-gaza.ts fetches
-  // killed-in-gaza.json at each of these to work out which update first
-  // introduced a given id. Until it is added, this update's 12,878 new records
-  // resolve to `update: -1` in the v3 API.
+  "a9fc873e39a8cbedc04e84098d0f4960c64807a5", // 10: July 2026 (IBC)
 ];
 
 export const updateDates = [
