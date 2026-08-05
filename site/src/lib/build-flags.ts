55c8a8f86c5d7a052ca7595a4b0538887eece53b
Wes Johnson <wes@swj.io>
2026-08-05 08:00:21 -0400

    surface stories by query param

--- site/src/lib/build-flags.ts
+++ site/src/lib/build-flags.ts
@@ -1,6 +1,5 @@
 const headerCTAs = false;
 const headerStats = false;
-const exploreStories = true;
 const legacyModules = false;
 
-export const BuildFlags = { headerCTAs, headerStats, exploreStories, legacyModules };
+export const BuildFlags = { headerCTAs, headerStats, legacyModules };
